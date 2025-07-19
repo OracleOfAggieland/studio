5d00bbe156945421726ed0ae243bc60240f94697
OracleOfAggieland <68983971+OracleOfAggieland@users.noreply.github.com>
2025-07-19 08:15:04 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'Habitual',
+  title: 'Habituality',
   description: 'Build and track your habits to achieve your goals.',
 };
 
